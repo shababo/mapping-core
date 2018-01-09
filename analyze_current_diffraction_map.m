07031d350426f4bb0facc61f6959046d813833dc
shababo <benshababo@gmail.com>
2018-01-08 16:21:09 -0800

    pos vs cur analysis

--- analyze_current_diffraction_map.m
+++ analyze_current_diffraction_map.m
@@ -1,7 +1,13 @@
 function result = analyze_current_diffraction_map(data,experiment_setup,trials)
 
 num_locs = length(trials);
-stim_starts = 20*[data.sequence.start];
+
 
 for i = 1:num_locs
-    result.traces{experiment_setup.pos_order(i)} = get_traces(data,trials(i),
+    
+    [curr_traces, ~,~, stim_traces] = get_traces(data,trials(i));
+    result.traces{experiment_setup.pos_order(i)} = curr_traces;
+    result.stim_size{experiment_setup.pos_order(i)} = mean(stim_traces(:,.0005*20000:.0025*20000),2);
+    result.max_curr{experiment_setup.pos_order(i)} = curr_traces(:,1) - min(curr_traces,[],2);
+    
+end
